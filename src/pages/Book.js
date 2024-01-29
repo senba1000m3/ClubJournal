196d4445b3e08bb70a416432cf3ead6a96719488
senba1000m3 <senba1000m3@gmail.com>
2024-01-29 14:11:15 +0800

    upadte flip animaion

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -1,11 +1,10 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import TextBlock from "./TextBlock";
 import EachPages from "./EachPages";
 import {motion} from "framer-motion";
 import {Route} from "react-router-dom";
 
-const FlipPage = ({pageIndex, front, back}) => {
-    const [nowPage, setNowPage] = useState([false]);
+const FlipPage = ({pageIndex, front, back, nowPage, setNowPage}) => {
 
     return (
         <motion.div layout className="pagebox"
@@ -30,19 +29,21 @@ const FlipPage = ({pageIndex, front, back}) => {
     );
 }
 const Book = () => {
-
+    const [nowPage, setNowPage] = useState([false]);
 
     return (
         <div className="book">
-            <div className="bookpage">
+            <div className="bookpage" style={{marginLeft: nowPage[1] ? '-800px':'0px'}}>
                 <FlipPage pageIndex={1}
                           front={<img src="/ClubJournal/pic/iriya.png" style={{width: '182mm', height: '257mm'}}/>}
                           back={<EachPages page={1}/>}
+                          nowPage={nowPage} setNowPage={setNowPage}
                 />
                 {Array.from({ length: 5 }, (_, index) => (
                     <FlipPage key = {index} pageIndex={index+2}
                               front={<EachPages page={2}/>}
                               back={<EachPages page={1}/>}
+                              nowPage={nowPage} setNowPage={setNowPage}
                     />
                 ))}
                 <FlipPage pageIndex={7}
@@ -50,6 +51,7 @@ const Book = () => {
                           back={<div style={{width: '182mm', height: '257mm', backgroundColor: "white", display: "flex", justifyContent: "center"}}>
                               <a style={{position: "absolute", bottom: '12px', fontSize: '24px'}}>© 2024 Senba</a>
                           </div>}
+                          nowPage={nowPage} setNowPage={setNowPage}
                 />
             </div>
         </div>
